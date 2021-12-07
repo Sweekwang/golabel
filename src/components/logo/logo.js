ce5c47f31b8e7afddfb193a4f8f32e97988a35aa
Swee Kwang Chua <sweekwang@Swees-MacBook-Pro.local>
2021-12-07 22:13:01 +0800

    Added all model

--- src/components/logo/logo.js
+++
@@ -1,7 +0,0 @@
-import Logo from '../../assets/logo.svg';
-
-const Icon = () => (
-    <img src={Logo} style={{boxSizing: "border-box", padding:"8px", height: "100%"}} alt="logo"/>
-);
-
-export default Icon;
