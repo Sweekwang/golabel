88de5d9c6ff4d5efac1cf37c84bac73402501e62
Swee Kwang Chua <sweekwang@Swees-MacBook-Pro.local>
2022-01-12 20:54:24 +0800

    Add color picker

--- src/components/network/network.js
+++ src/components/network/network.js
@@ -1,17 +1,62 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import ReactDOM from 'react-dom';
 import CytoscapeComponent from 'react-cytoscapejs';
 import AButton from '../button/aButton';
 import classes from './network.module.css';
+import { CompactPicker } from 'react-color';
+import { Button } from '@material-ui/core';
+
 
 const Network = (props) => {
 
   let [x, setX] = useState(window.innerWidth / 2.5);
   let [sizeHeight, setSizeHeight] = useState('500px');
   let [text, setText] = useState('Increase');
+  let [changeNode, setChangeNode] = useState(false);
+
+  let [GO_biological_process, setGO_biological_process] = useState("rgb(228,185,165)");
+  let [DGE_infection_and_immunity, setDGE_infection_and_immunity] = useState("rgb(228,185,165)");
+  let [Tandemly, setTandemly] = useState("rgb(155,95,55)");
+  let [Regulatory, setRegulatory] = useState("rgb(240,2,127)");
+  let [Conservation, setDGE_general] = useState("rgb(190,174,212)");
+  let [molecular, setmolecular] = useState("rgb(203,178,196)");
+  let [PPI, setPPI] = useState("rgb(56,108,176)");
+  let [Coexp, setCoexp] = useState("gb(165,185,178)");
+  let [DGE_growth, setDGE_growth] = useState("#ffffff");
+  let [domains, setdomains] = useState("#ffffff");
+  let [network, setnetwork] = useState("#ffffff");
+  let [DGE_stress, setDGE_stress] = useState("#ffffff");
+  let [Pfam, setPfam] = useState("#ffffff");
+  let [Diurnal, setDiurnal] = useState("#ffffff");
+  let [methlyated, setmethlyated] = useState("#ffffff");
+  let [TF_TG, setTF_TG] = useState("#ffffff");
+  let [Transmembrane, setTransmembrane] = useState("#ffffff");
+  let [GO_molecular_function, setGO_molecular_function] = useState("#ffffff");
+  let [Single, setSingle] = useState("#ffffff");
+  let [Aranet, setAranet] = useState("#ffffff");
+  let [DGE_light, setDGE_light] = useState();
+  let [GO_cellular_component, setGO_cellular_component] = useState("#ffffff");
+  let [Aranet2, setAranet2] = useState("#ffffff");
+  let [Homolog, setHomolog] = useState("#ffffff");
+  let [element, setelement] = useState("#ffffff");
+  let [Phylostrata, setPhylostrata] = useState("#ffffff");
+  let [SPM, setSPM] = useState("#ffffff");
+  let [PPInetwork, setPPInetwork] = useState("#ffffff");
+  let [TWAS, setTWAS] = useState("#ffffff");
+  let [families, setfamilies] = useState("#ffffff");
+  let [Disordered, setDisordered] = useState("#ffffff");
+  let [Orthogroups, setOrthogroups] = useState("#ffffff");
+  let [Regulatory2, setRegulatory2] = useState("#ffffff");
+  let [Biochemical, setBiochemical] = useState("#ffffff");
+  let [PTMs, setPTMs] = useState("#ffffff");
+  let [TPM, setTPM] = useState("#ffffff");
+
+
+
 
      const layout = {
-        name: 'random'
+        name: 'random', //'cose', 'random', 'breadthfirst', circle, grid
+        animate: true,
       }
 
       const stylesheet = [
@@ -22,7 +67,7 @@ const Network = (props) => {
             "background-opacity" : 1.0,
             "border-opacity" : 1.0,
             "font-size" : 12,
-            backgroundColor: "rgb(137,208,245)",
+            backgroundColor: "#FF6363",
             "text-valign" : "center",
             "text-halign" : "center",
             "color" : "rgb(0,0,0)",
@@ -39,42 +84,42 @@ const Network = (props) => {
         {
           selector: "node[feat_category = 'GO_biological_process']",
           style : {
-            backgroundColor: "rgb(254,230,145)",
+            backgroundColor: GO_biological_process,
           }
         }, {
           selector: "node[feat_category = 'DGE_infection and immunity']",
           style : {
-            backgroundColor: "rgb(228,185,165)"
+            backgroundColor: DGE_infection_and_immunity
           }
         }, {
           selector: "node[feat_category = 'Tandemly duplicated']",
           style : {
-            backgroundColor: "rgb(155,95,55)"
+            backgroundColor: Tandemly
           }
         }, {
           selector: "node[feat_category = 'Regulatory clusters']",
           style : {
-            backgroundColor: "rgb(240,2,127)"
+            backgroundColor: Regulatory
           }
         }, {
           selector: "node[feat_category = 'Conservation features']",
           style : {
-            backgroundColor: "rgb(190,174,212)"
+            backgroundColor: Conservation
           }
         }, {
           selector: "node[feat_category = 'DGE_general molecular function']",
           style : {
-            backgroundColor: "rgb(203,178,196)"
+            backgroundColor: molecular
           }
         }, {
           selector: "node[feat_category = 'PPI clusters']",
           style : {
-            backgroundColor: "rgb(56,108,176)"
+            backgroundColor: PPI
           }
         }, {
           selector: "node[feat_category = 'Coexp clusters']",
           style : {
-            backgroundColor: "rgb(165,185,178)"
+            backgroundColor: Coexp
           }
         }, {
           selector: "node[feat_category = 'DGE_growth and development']",
@@ -311,22 +356,175 @@ const Network = (props) => {
         }
         
       });
+    }
 
+    const updateColor = (color) => {
+      console.log(color)
+      setAranet(color.hex)
+    }
+
+    const changeNodeColorHandler = () => {
+      setChangeNode(!changeNode)
     }
 
     return(
+      <Fragment>
+        <div className={classes.buttondiv}>
+          <AButton onClick={changeNodeColorHandler}>Change Node Color</AButton>
+          <AButton>Change Edge Color</AButton>
+        </div>
+        {changeNode && <div className={classes.colorContainer}>
+          <div className={classes.colorContainerItem}>
+            <p>GO_biological_process:</p>
+            <CompactPicker color={GO_biological_process} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>DGE_infection and immunity:</p>
+            <CompactPicker color={DGE_infection_and_immunity} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Tandemly duplicated:</p>
+            <CompactPicker color={Tandemly} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Regulatory clusters:</p>
+            <CompactPicker color={Regulatory} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Conservation features:</p>
+            <CompactPicker color={Conservation} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>DGE_general molecular function:</p>
+            <CompactPicker color={molecular} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>PPI clusters:</p>
+            <CompactPicker color={PPI} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Coexp network features:</p>
+            <CompactPicker color={Coexp} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>DGE_growth and development:</p>
+            <CompactPicker color={DGE_growth} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Number of domains:</p>
+            <CompactPicker color={domains} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Coexp network features:</p>
+            <CompactPicker color={network} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>DGE_stress and stimulus:</p>
+            <CompactPicker color={DGE_stress} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Pfam domains:</p>
+            <CompactPicker color={Pfam} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Diurnal timepoints:</p>
+            <CompactPicker color={Diurnal} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Gene body methlyated:</p>
+            <CompactPicker color={methlyated} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>TF-TG properties:</p>
+            <CompactPicker color={TF_TG} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Transmembrane helices:</p>
+            <CompactPicker color={Transmembrane} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>GO_molecular_function:</p>
+            <CompactPicker color={GO_molecular_function} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Single copy:</p>
+            <CompactPicker color={Single} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Aranet network features:</p>
+            <CompactPicker color={Aranet} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>DGE_light and circadian:</p>
+            <CompactPicker color={DGE_light} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>GO_cellular_component:</p>
+            <CompactPicker color={GO_cellular_component} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Aranet clusters:</p>
+            <CompactPicker color={Aranet2} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Homolog features:</p>
+            <CompactPicker color={Homolog} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>cis-regulatory element names:</p>
+            <CompactPicker color={element} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Phylostrata:</p>
+            <CompactPicker color={Phylostrata} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>SPM features:</p>
+            <CompactPicker color={SPM} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>PPI network features:</p>
+            <CompactPicker color={PPInetwork} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>TWAS features:</p>
+            <CompactPicker color={TWAS} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>cis-regulatory element families</p>
+            <CompactPicker color={families} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Disordered domains regions:</p>
+            <CompactPicker color={Disordered} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Orthogroups:</p>
+            <CompactPicker color={Orthogroups} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Regulatory network features':</p>
+            <CompactPicker color={Regulatory2} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Biochemical features:</p>
+            <CompactPicker color={Biochemical} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>Protein PTMs:</p>
+            <CompactPicker color={PTMs} onChangeComplete={updateColor}/>
+          </div>
+          <div className={classes.colorContainerItem}>
+            <p>TPM features:</p>
+            <CompactPicker color={TPM} onChangeComplete={updateColor}/>
+          </div>
+        </div>}
         <div className={classes.network}>
           <CytoscapeComponent 
               elements={props.elements} 
               style={ {  
                 height: sizeHeight,
               } } 
-              cy={cy =>
-                cy.on('add', 'node', _evt => {
-                    cy.layout(layout).run()
-                    cy.fit()
-                })
-            }
               stylesheet={stylesheet}
               layout={layout} 
               pan={ { x: x, y: 80 } }
@@ -337,7 +535,8 @@ const Network = (props) => {
               />
           <AButton className={classes.btn} onClick={largehandler}>{text} Frame Height</AButton>
           
-        </div>    
-        )
+        </div> 
+      </Fragment>   
+    )
 }
 export default Network;
