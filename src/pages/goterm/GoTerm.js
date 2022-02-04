1710f51c354abec5aac15eee741d859f7738108d
Swee Kwang Chua <sweekwang@Swees-MacBook-Pro.local>
2022-02-04 10:42:04 +0800

    commit to checkout

--- src/pages/goterm/GoTerm.js
+++ src/pages/goterm/GoTerm.js
@@ -32,7 +32,7 @@ const GoTerm = () => {
     const data = { labels: goId };
     //console.log(data);
 
-    fetch('https://go-label-316405.oa.r.appspot.com/api/go', {
+    fetch('http://127.0.0.1:5000/api/go', {
       method: 'POST',
       body: JSON.stringify(data),
       mode: 'cors',
