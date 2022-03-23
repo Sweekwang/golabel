71c195c9e6c226faefe4e9493e06df3a176734d2
Jonathan Ng <weixiong001@e.ntu.edu.sg>
2022-03-23 19:00:01 +0800

    recreated all_models branch with a clean slate

--- src/components/button/aButton.js
+++
@@ -1,9 +0,0 @@
-import classes from './aButton.module.css';
-
-const AButton = (props) => {
-    return(
-        <a href={props.href} className={[classes.aButton, props.className].join(' ')} onClick={props.onClick}>{props.children}</a>
-    )
-};
-
-export default AButton;
